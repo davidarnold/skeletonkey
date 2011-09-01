8151d034a0bd136eac60a6f593e7da2c5b4d6c88
David Arnold <david.n.arnold@gmail.com>
2011-09-01 02:05:34 -0400

    better URI encoding

--- build.js
+++ build.js
@@ -6,6 +6,4 @@ file = fs.readFileSync('skeletonkey.js', 'utf8');
 ast = ujs.parser.parse(file);
 ast = ujs.uglify.ast_mangle(ast);
 ast = ujs.uglify.ast_squeeze(ast);
-process.stdout.write('javascript:' + encodeURI(ujs.uglify.gen_code(ast)) + '\n');
-
-
+process.stdout.write('javascript:' + encodeURIComponent(ujs.uglify.gen_code(ast)) + '\n');
